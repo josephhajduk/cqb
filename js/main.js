6decafdd8bb920b5b568e439b32134bddb8367ab
Joseph Hajduk <joseph.hajduk@gmail.com>
2012-11-20 16:17:13 -0700

    added badlands sample stuff to test cocoonjs

--- js/main.js
+++ js/main.js
@@ -5,63 +5,82 @@
     
         var bg = new CAAT.ActorContainer().
                 setBounds(0,0,director.width,director.height).
-                setFillStyle('#222');
+                setFillStyle('#000');
     
         scene.addChild(bg);
-    
-        // create a sprite image of 1 row by 6 columns
-        var starsImage= new CAAT.SpriteImage().
-                initialize(director.getImage('stars'), 1,6 );
-    
-        var T= 1000;
-    
-        var mouseStars= function(mouseEvent) {
-    
-            var actorStar= new CAAT.Actor().
-                // set background image to be a reference of a SpriteImage instance
-                // and set actor's size equal to a SpriteImage's subimage size
-                    setBackgroundImage(
-                        starsImage.getRef(), true ).
-                // set background as a random SpriteImage's subimage.
-                    setSpriteIndex( (Math.random()*6)>>0 ).
-                // center the actor on mouse position
-                    centerOn( mouseEvent.point.x, mouseEvent.point.y).
-                // when the actor expires, remove in from the director
-                    setDiscardable(true).
-                // avoid mouse event handling.
-                    enableEvents(false).
-                // make this actor last to T milliseconds (1000)
-                    setFrameTime(scene.time, T).
-                // add a scaling behavior
-                    addBehavior(
-                        new CAAT.ScaleBehavior().
-                            setFrameTime(scene.time, T).
-                            setValues( 1,5, 1,5 ).
-                            setInterpolator(
-                                new CAAT.Interpolator().createExponentialInInterpolator(
-                                    3,
-                                    false)
-                            )
-                    ).
-                // add an alpha behavior so the actor takes 1000 ms to fade out to zero alpha
-                    addBehavior(
-                        new CAAT.AlphaBehavior().
-                            setFrameTime(scene.time, T).
-                            setValues( 1, 0 ) );
-    
-            // add the actor.
-            bg.addChild(actorStar);
-        };
-    
-        // set background's mouse handlers.
-        bg.mouseMove= mouseStars;
-        bg.mouseDrag= mouseStars;
+        
+        var badImage = new CAAT.SpriteImage().initialize(director.getImage('splash_bad'),1,1);
+        var landsImage = new CAAT.SpriteImage().initialize(director.getImage('splash_lands'),1,1);
+        var carImage = new CAAT.SpriteImage().initialize(director.getImage('splash_car'),1,1);
+        var splashImage = new CAAT.SpriteImage().initialize(director.getImage('splash_splash'),1,1);
+
+        var actorBad = new CAAT.Actor()
+            .setBackgroundImage(badImage.getRef(),true)
+            .addBehavior(
+                new CAAT.AlphaBehavior()
+                    .setFrameTime(scene.time,1000)
+                    .setValues(0,1)
+                );  
+                      
+        var actorLands = new CAAT.Actor()
+            .setBackgroundImage(landsImage.getRef(),true)
+            .setAlpha(0)
+            .addBehavior(
+                new CAAT.AlphaBehavior()
+                    .setFrameTime(scene.time+500,1500)
+                    .setValues(0,1)
+                );
+                
+            
+        var carPath = new CAAT.Path().setLinear(350,12, 10,12); 
+        var yellowPath = new CAAT.Path().setLinear(350,21, 10,21); 
+        
+        var actorCarYellow = new CAAT.Actor()
+            .centerAt(350,20)
+            .setSize(400,6)
+            .setFillStyle("#fbcb00")
+            .addBehavior(
+                new CAAT.PathBehavior()
+                    .setFrameTime(scene.time+1000,1000)
+                    .setPath(yellowPath)
+                );   
+            
+        
+        var actorCar = new CAAT.Actor()
+            .centerAt(350,20)
+            .setBackgroundImage(carImage.getRef(),true)
+            .addBehavior(
+                new CAAT.PathBehavior()
+                    .setFrameTime(scene.time+1000,1000)
+                    .setPath(carPath)
+                );   
+                
+        var actorSplash = new CAAT.Actor()
+            .setAlpha(0)
+            .setBackgroundImage(splashImage.getRef(),true)
+            .addBehavior(
+                new CAAT.AlphaBehavior()
+                    .setFrameTime(scene.time+2000,2000)
+                    .setValues(0,1)
+            );
+                
+        bg.addChild(actorBad)
+        bg.addChild(actorLands)
+        bg.addChild(actorCarYellow)
+        bg.addChild(actorCar)
+        bg.addChild(actorSplash)
+            
     }
     
     function __init()   {
-    
+        CAAT.DEBUG=1;
+        
+        canvas = document.createElement("canvas");
+        canvas.setAttribute("id","main");
+        document.getElementById("screen_area").appendChild(canvas)
+        
         var director = new CAAT.Director().
-                initialize(1600,900,document.getElementById('main')).
+                initialize(336,240,document.getElementById('main')).
                 setClear(false);
     
         director.enableResizeEvents(16);
@@ -69,7 +88,7 @@
         $(window).load(function() {
             $(window).bind('resize',function(event){
                 window.scrollTo(0,0);
-                director.windowResized($(window).width() - 15, $(window).height() - 15);
+                director.windowResized($(window).width() - 30, $(window).height() - 30);
                 
                 topOffset = Math.floor(($(window).height() - $("#main").height())/2)
                 leftOffset = Math.floor(($(window).width() - $("#main").width())/2)
@@ -94,8 +113,11 @@
         });
     
         new CAAT.ImagePreloader().loadImages(
-            [
-                {id:'stars',    url:'http://labs.hyperandroid.com/static/CAAT-Samples/demos/demo-resources/img/stars.png'}
+            [   
+                {id:'splash_bad',    url:'img/bad.png'},
+                {id:'splash_lands',    url:'img/lands.png'},
+                {id:'splash_car',    url:'img/car.png'},
+                {id:'splash_splash',    url:'img/splash.png'},
             ],
             function( counter, images ) {
                 director.setImagesCache(images);
